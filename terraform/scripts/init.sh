cbdf876612f051488f7aefd08be4d4c83790119f
Matias Gimenez <matiasgimenez.dev@gmail.com>
2024-05-16 12:59:01 -0300

    update image names

--- terraform/scripts/init.sh
+++ terraform/scripts/init.sh
@@ -1,2 +1,2 @@
-sudo docker pull fedesin31/sobel-service:latest
-sudo docker run -p 80:5000 --name sobel fedesin31/sobel-service:latest
+sudo docker pull mgimenezdev/sobel-service:latest
+sudo docker run -p 80:5000 --name sobel mgimenezdev/sobel-service:latest
